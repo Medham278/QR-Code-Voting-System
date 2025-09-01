8f0b64f370c22e6d844ac7c1f9c10b4d217d24c7
Medha Mathur <58322032+Medham278@users.noreply.github.com>
2025-09-02 00:58:15 +0530

    Update settings.py

--- QRvote/QRvote/settings.py
+++ QRvote/QRvote/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*$gx-2y184+s4jy4+#_q72)i6ar-f!1qlb(5ahm58ike3mrtv('
+SECRET_KEY = '##############'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
