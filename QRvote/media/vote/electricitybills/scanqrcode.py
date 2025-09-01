981f310d3eb2568836a5379e59dd96257108a13d
Medha Mathur <58322032+Medham278@users.noreply.github.com>
2025-09-02 00:43:01 +0530

    Add files via upload

---
+++ QRvote/media/vote/electricitybills/scanqrcode.py
@@ -0,0 +1,22 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+capture=cv2.VideoCapture(0)
+received_data=None
+while True:
+    _,frame=capture.read()
+    decoded_data=decode(frame)
+    try:
+        data=decoded_data[0][0]
+        if data != received_data:
+            print(data)
+            received_data=data
+    except:
+        pass
+    cv2.imshow("qr code scanner",frame)
+    key=cv2.waitKey(1)
+    if key== ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
+        
